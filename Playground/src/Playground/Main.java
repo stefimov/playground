79082e75d6c8034f8bff68020097d89a30c04841
STAS <STAS@STAS-PC>
2018-03-08 15:34:45 +0300

    new output command

--- Playground/src/Playground/Main.java
+++ Playground/src/Playground/Main.java
@@ -3,6 +3,8 @@ package Playground;
 public class Main {
 	public static void main(String[] args)
 	{
+		//Greeting message
 		System.out.println("Hello, World!\nGit too!\nYeah!");
+		System.out.println("Test");
 	}
 }
